33c669a5a47d33a88bad3279a7f475a9b3410de4
sampi <sampi@mybox.(none)>
2013-10-11 10:43:26 +0200

    first commit

---
+++ Rakefile
@@ -0,0 +1,17 @@
+#!/usr/bin/env rake
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require File.expand_path('../config/application', __FILE__)
+
+require 'rake'
+require 'rake/dsl_definition'
+
+Myapp::Application.load_tasks
+
+module ::Myapp
+  class Application
+    include Rake::DSL
+  end
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '8d862a527b63cda7aa69a2c2ee4107737feb35bc455320cdc884b0c10337db35b9d0f6cf57f50cef2a2c29c49b767b5b8197f1f03814c9ca3eb7269c4adadd77'
